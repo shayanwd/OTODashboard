1a444775242d37059a5044e84b1c0831e777b450
Shayan Khan <shayankhanwd@gmail.com>
2020-09-06 05:01:06 -0700

    asd

--- checking.php
+++
@@ -1,5 +0,0 @@
-if you can to upload the schnages to server first commint the changes like this..
-
-now you can see 0 down and 1 up  this means you have 1 chnage that you have to uploaded yet 
-
-no press sync that round button to upload
