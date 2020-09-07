cbf2d125b74279c35174d79fa95cc7e0474c2e1f
Noor <noorcompany3@gmail.com>
2020-09-07 15:48:45 +0500

    as

--- Organization-Management.php
+++ Organization-Management.php
@@ -33,11 +33,11 @@ include('./header.php')
 							<div class="card-body">
                                 <div class="card-head">
                                     <div class="row">
-								    <div class="col-md-6 ">
-                                        <div class="heading_Id">
-                                            <p>Organization Management /<span>Details (#234234)</span></p>
-                                        </div>
-                                    </div>
+										<div class="col-md-6 ">
+											<div class="heading_Id">
+												<p>Organization Management /<span>Details (#234234)</span></p>
+											</div>
+										</div>
                                     <div class="col-md-6">
                                         <div class="row">
                                             <div class="col-md-4">
@@ -83,6 +83,28 @@ include('./header.php')
                                     </div>
                                 </div>
                             </div>
+							<div class="row">
+								<div class="col-md-12">
+									<div class="summery_tabs">
+										<ul class="nav nav-tabs" id="myTab" role="tablist">
+  											<li class="nav-item" role="presentation">
+    											<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+  											</li>
+  											<li class="nav-item" role="presentation">
+    											<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+  											</li>
+  											<li class="nav-item" role="presentation">
+    											<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+  											</li>
+										</ul>
+										<div class="tab-content" id="myTabContent">
+  											<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  											<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  											<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+										</div>
+									</div>
+								</div>
+							</div>
                                 
                             </div>
                         </div>
