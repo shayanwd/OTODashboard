42fd4b74761a6a84f96c0e526fd0466c85bc7fab
Noor <noorcompany3@gmail.com>
2020-09-07 16:07:50 +0500

    as

--- Organization-Management.php
+++ Organization-Management.php
@@ -88,7 +88,7 @@ include('./header.php')
 									<div class="summery_tabs">
 										<ul class="nav nav-tabs" id="myTab" role="tablist">
   											<li class="nav-item" role="presentation">
-    											<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+    											<a class="nav-link active" id="SUMMARY-tab" data-toggle="tab" href="#SUMMARY" role="tab" aria-controls="SUMMARY" aria-selected="true">SUMMARY</a>
   											</li>
   											<li class="nav-item" role="presentation">
     											<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
@@ -98,7 +98,7 @@ include('./header.php')
   											</li>
 										</ul>
 										<div class="tab-content" id="myTabContent">
-  											<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  											<div class="tab-pane fade show active" id="SUMMARY" role="tabpanel" aria-labelledby="SUMMARY-tab">...</div>
   											<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
   											<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
 										</div>
