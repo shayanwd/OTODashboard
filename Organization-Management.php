b9cdd721a709183a551ba3237e8fc09740e01c56
Shayan Khan <shayankhanwd@gmail.com>
2020-09-07 08:38:45 -0700

    s

--- Organization-Management.php
+++ Organization-Management.php
@@ -2,8 +2,9 @@
 <html lang="en">
 
 <head>
-    <title>OTO| Organization-Management</title>
+	<title>OTO| Organization-Management</title>
 </head>
+
 <body class="">
 	<!-- [ Pre-loader ] start -->
 	<div class="loader-bg">
@@ -15,142 +16,137 @@
 	<!-- [ Mobile header ] start -->
 
 	<?php
-include('./header.php')
-?>
+	include('./header.php')
+	?>
 
 
 	<!-- [ Header ] end -->
 
-<!-- [ Main Content ] start -->
-<div class="pc-container">
-    <div class="pcoded-content">
-        <div class="row">
-            <!-- support-section start -->
-            <div class="col-xl-12 col-md-12">
-                <div class="row">
-                    <div class="col-md-12">
-						<div class="card_my prod-p-card background-pattern">
-							<div class="card-body">
-                                <div class="card-head">
-                                    <div class="row">
-										<div class="col-md-6 ">
-											<div class="heading_Id">
-												<p>Organization Management /<span>Details (#234234)</span></p>
+	<!-- [ Main Content ] start -->
+	<div class="pc-container">
+		<div class="pcoded-content ">
+			<div class="row">
+				<!-- support-section start -->
+				<div class="col-xl-12 col-md-12">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="card_my prod-p-card background-pattern">
+								<div class="card-body remove-pb">
+									<div class="card-head">
+										<div class="row">
+											<div class="col-md-6 ">
+												<div class="heading_Id">
+													<p>Organization Management /<span>Details (#234234)</span></p>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="row">
+													<div class="col-md-4">
+														<div class="sticker">
+															<a href="#">
+																<img src="assets/images/sticker_1.png" />
+															</a>
+														</div>
+													</div>
+													<div class="col-md-4">
+														<div class="sticker">
+															<a href="#">
+																<img src="assets/images/sticker_2.png" />
+															</a>
+														</div>
+													</div>
+													<div class="col-md-4">
+														<div class="sticker_1">
+															<a href="#" class="btn_light_1">
+																<p>Profile Status</p><span>Active</span><i class="fas fa-chevron-down"></i>
+															</a>
+														</div>
+													</div>
+												</div>
 											</div>
 										</div>
-                                    <div class="col-md-6">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <div class="sticker">
-												<a href="#">
-                                                   <img src="assets/images/sticker_1.png"/>
-												   </a>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <div class="sticker">
-												<a href="#">
-                                                <img src="assets/images/sticker_2.png"/>
-												</a>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <div class="sticker_1">
-                                                    <a href="#" class="btn_light_1"><p>Profile Status</p><span>Active</span><i class="fas fa-chevron-down"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            <div class="row dec">
-                                <div class="col-md-2">
-                                    <div class="e-logo">
-                                        <img src="assets/images/e-logo.png">
-                                    </div>
-                                </div>
-                                <div class="col-md-10">
-                                    <div class="logo-decription">
-                                        <div class="logo_dec">
-                                            <h5>E:Skoot<span>United Kindom</span></h5>
-                                        </div>
-                                        <div class="contact-info">
-                                            <ul>
-                                                <li><img src="assets/images/mess.png">Eskoot@yahoo.com</li>
-                                                <li><img src="assets/images/phone.png">997278889</li>
-                                                <li><img src="assets/images/globe.png">www.eskoot.com</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-							<div class="row">
-								<div class="col-md-12">
-									<!-- <div class="summery_tabs">
-										<ul class="nav nav-tabs" id="myTab" role="tablist">
-  											<li class="nav-item" role="presentation">
-    											<a class="nav-link active" id="SUMMARY-tab" data-toggle="tab" href="#SUMMARY" role="tab" aria-controls="SUMMARY" aria-selected="true">SUMMARY</a>
-  											</li>
-  											<li class="nav-item" role="presentation">
-    											<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-  											</li>
-  											<li class="nav-item" role="presentation">
-    											<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-  											</li>
-										</ul>
-										<div class="tab-content" id="myTabContent">
-  											<div class="tab-pane fade show active" id="SUMMARY" role="tabpanel" aria-labelledby="SUMMARY-tab">...</div>
-  											<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-  											<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+										<div class="row dec">
+											<div class="col-md-2">
+												<div class="e-logo">
+													<img src="assets/images/e-logo.png">
+												</div>
+											</div>
+											<div class="col-md-10">
+												<div class="logo-decription">
+													<div class="logo_dec">
+														<h5>E:Skoot<span>United Kindom</span></h5>
+													</div>
+													<div class="contact-info">
+														<ul>
+															<li><img src="assets/images/mess.png">Eskoot@yahoo.com</li>
+															<li><img src="assets/images/phone.png">997278889</li>
+															<li><img src="assets/images/globe.png">www.eskoot.com</li>
+														</ul>
+													</div>
+												</div>
+											</div>
 										</div>
-									</div> -->
-<!-- ===================================================================================================
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\page-tabs-start\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-=================================================================================================== -->
-<!-- Tab links -->
-<div class="page-tab">
-  <button class="tablinks active" onclick="openCity(event, 'London')">London</button>
-  <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
-  <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
-</div>
-
-<!-- Tab content -->
-<div id="London" class="tabcontent">
-  <h3>London</h3>
-  <p>London is the capital city of England.</p>
-</div>
-
-<div id="Paris" class="tabcontent">
-  <h3>Paris</h3>
-  <p>Paris is the capital of France.</p>
-</div>
-
-<div id="Tokyo" class="tabcontent">
-  <h3>Tokyo</h3>
-  <p>Tokyo is the capital of Japan.</p>
-</div>
+										<div class="row">
+											<div class="col-md-12">
 
 
-<!-- ===================================================================================================
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\page-tabs-end\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-=================================================================================================== -->
- 
+												<!-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\page-tabs-start\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ -->
+
+												<!-- Tab links -->
+												<div class="page-tab">
+													<button class="tablinks active" onclick="openCity(event, 'page-tab-content1')">SUMMARY</button>
+													<button class="tablinks" onclick="openCity(event, 'page-tab-content2')">BASIC PROFILE</button>
+													<button class="tablinks" onclick="openCity(event, 'page-tab-content3')">ASSIGNED FLEETS</button>
+												</div>
 
 
-
+												<!-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\page-tabs-end\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ -->
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
-                                
-                            </div>
-                        </div>
+						</div>
 					</div>
 				</div>
 			</div>
-        </div>
+		</div>
+
+		<!-- Tab content -->
+		<div id="page-tab-content1" class="tabcontent active">
+			<div class="row">
+				<div class="col-md-12 col-xl-12">
+					<div class="card_my prod-p-card background-pattern">
+						<div class="card-body">
+							<div class="card-head">
+								<h1 class="text-center">Graphs-pending....</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="page-tab-content2" class="tabcontent">
+			<h3>Paris</h3>
+			<p>Paris is the capital of France.</p>
+		</div>
+
+		<div id="page-tab-content3" class="tabcontent">
+			<h3>Tokyo</h3>
+			<p>Tokyo is the capital of Japan.</p>
+		</div>
+
 	</div>
-</div>
-</div>
-			<!-- [ table Content ] start -->	
-			<!-- <div class="row">
+	<!-- [ table Content ] start -->
+
+
+
+
+
+
+
+	<!-- <div class="row">
 			<div class="col-xl-12 col-md-12">
 				<div class="card">
 					<div class="card-body_my">
@@ -238,19 +234,19 @@ include('./header.php')
 				</div>
 			</div>				 -->
 
-<!-- [ table Content ] end -->
-    <script src="assets/js/vendor-all.min.js"></script>
-    <script src="assets/js/plugins/bootstrap.min.js"></script>
-    <script src="assets/js/plugins/feather.min.js"></script>
-    <script src="assets/js/pcoded.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
-    <script src="assets/js/plugins/clipboard.min.js"></script>
-    <script src="assets/js/uikit.min.js"></script>
+	<!-- [ table Content ] end -->
+	<script src="assets/js/vendor-all.min.js"></script>
+	<script src="assets/js/plugins/bootstrap.min.js"></script>
+	<script src="assets/js/plugins/feather.min.js"></script>
+	<script src="assets/js/pcoded.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+	<script src="assets/js/plugins/clipboard.min.js"></script>
+	<script src="assets/js/uikit.min.js"></script>
 
-<!-- Apex Chart -->
-<script src="assets/js/plugins/apexcharts.min.js"></script>
-<!-- custom-chart js -->
-<script src="assets/js/pages/dashboard.js"></script>
+	<!-- Apex Chart -->
+	<script src="assets/js/plugins/apexcharts.min.js"></script>
+	<!-- custom-chart js -->
+	<script src="assets/js/pages/dashboard.js"></script>
 </body>
 
 </html>
