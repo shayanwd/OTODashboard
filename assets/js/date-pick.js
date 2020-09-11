7c9ef36d8210064cc68f8d503b562ef7cc866d8e
Shayan Khan <shayankhanwd@gmail.com>
2020-09-11 10:23:39 -0700

    oto

--- assets/js/date-pick.js
+++ assets/js/date-pick.js
@@ -29,6 +29,10 @@ $(document).ready(function(){
   cb(start, end);
 })
 
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
 $(document).ready(function(){
   
   var start = moment().subtract(29, 'days');
@@ -39,6 +43,36 @@ $(document).ready(function(){
   }
 
   $('#reportrange2').daterangepicker({
+      startDate: start,
+      endDate: end,
+      "opens": "left",
+    
+      "alwaysShowCalendars": true,
+      ranges: {
+         'Today': [moment(), moment()],
+         'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+         'This Month': [moment().startOf('month'), moment().endOf('month')],
+         'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      }
+  }, cb);
+
+  cb(start, end);
+})
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+$(document).ready(function(){
+  
+  var start = moment().subtract(29, 'days');
+  var end = moment();
+
+  function cb(start, end) {
+      $('#reportrange3 span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+  }
+
+  $('#reportrange3').daterangepicker({
       startDate: start,
       endDate: end,
       "opens": "left",
